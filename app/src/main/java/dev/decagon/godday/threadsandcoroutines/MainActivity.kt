8877a5e137d9784b03b6bd5d8d0b8a0ee4acb568
Godday Okoduwa <goddayokos@gmail.com>
2021-07-14 23:04:19 +0100

    Moved network operations to a different thread

--- app/src/main/java/dev/decagon/godday/threadsandcoroutines/MainActivity.kt
+++ app/src/main/java/dev/decagon/godday/threadsandcoroutines/MainActivity.kt
@@ -21,18 +21,26 @@ class MainActivity : AppCompatActivity() {
 
         // Your code
 
-        // Create and open a URL connection and cast it as HttpURLConnection
-        val imageUrl = URL("https://www.pngkit.com/png/detail/17-176284_eagle-owl-png-transparent-image-owl-png.png")
-        val connection = imageUrl.openConnection() as HttpURLConnection
-        connection.doInput = true // This connection is for input only
-        connection.connect()
+        /**
+         * Android forbids making network calls or connections on MainThread in
+         * other to avoid blocking it. So to download our image, we have to switch the
+         * operation to another thread (Background Thread or Worker Thread)
+         */
+        Thread {
+            // Create and open a URL connection and cast it as HttpURLConnection
+            val imageUrl = URL("https://www.pngkit.com/png/detail/17-176284_eagle-owl-png-transparent-image-owl-png.png")
+            val connection = imageUrl.openConnection() as HttpURLConnection
+            connection.doInput = true // This connection is for input only
+            connection.connect()
 
-        // Create an inputStream to read the image from the server to your app
-        val inputStream = connection.inputStream
-        // Decode the inputStream into a bitmap using BitmapFactory
-        val bitmap = BitmapFactory.decodeStream(inputStream)
+            // Create an inputStream to read the image from the server to your app
+            val inputStream = connection.inputStream
+            // Decode the inputStream into a bitmap using BitmapFactory
+            val bitmap = BitmapFactory.decodeStream(inputStream)
 
-        // Display the image
-        image.setImageBitmap(bitmap)
+            // Display the image
+            image.setImageBitmap(bitmap)
+        }
+
     }
 }
