2d21e446983aec5675e73408bdb69ca176e7ced1
Godday Okoduwa <goddayokos@gmail.com>
2021-07-15 03:07:56 +0100

    Launched a GlobalCoroutineScope and moved the background operations into it

--- app/src/main/java/dev/decagon/godday/threadsandcoroutines/MainActivity.kt
+++ app/src/main/java/dev/decagon/godday/threadsandcoroutines/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import java.net.HttpURLConnection
 import java.net.URL
 
@@ -13,6 +16,7 @@ import java.net.URL
  */
 class MainActivity : AppCompatActivity() {
 
+    @DelicateCoroutinesApi
     override fun onCreate(savedInstanceState: Bundle?) {
         // Switch to AppTheme for displaying the activity
         setTheme(R.style.AppTheme)
@@ -23,12 +27,7 @@ class MainActivity : AppCompatActivity() {
         // Get a reference to the MainLooper
         val mainLooper = mainLooper  // or Looper.getMainLooper()
 
-        /**
-         * Android forbids making network calls or connections on MainThread in
-         * other to avoid blocking it. So to download our image, we have to switch the
-         * operation to another thread (Background Thread or Worker Thread)
-         */
-        Thread {
+        GlobalScope.launch {
             // Create and open a URL connection and cast it as HttpURLConnection
             val imageUrl = URL("https://www.pngkit.com/png/detail/17-176284_eagle-owl-png-transparent-image-owl-png.png")
             val connection = imageUrl.openConnection() as HttpURLConnection
@@ -41,17 +40,6 @@ class MainActivity : AppCompatActivity() {
             val bitmap = BitmapFactory.decodeStream(inputStream)
 
             /**
-             * Only the MainThread (UI Thread) can update the UI in Android
-             * so we switch to the UI thread to display our image i.e
-             * we post the data to the UI thread. There are several methods used
-             * in doing this.
-             */
-
-            // 1. This method is only available inside an activity
-        //    runOnUiThread { image.setImageBitmap(bitmap) }
-
-            // 2. Using Handler with MainLooper
-            /**
              * Loopers are pieces of the Android ecosystem which loops through all
              * messages or signals any thread receives and process them.
              * The MainThread Looper or simply MainLooper handles messages for the
@@ -63,8 +51,73 @@ class MainActivity : AppCompatActivity() {
              * between signals and threads that should process them
              */
             Handler(mainLooper).post { image.setImageBitmap(bitmap) }
+        }
 
-        }.start()  // Start the thread
+        /**
+         * Android forbids making network calls or connections on MainThread in
+         * other to avoid blocking it. So to download our image, we have to switch the
+         * operation to another thread (Background Thread or Worker Thread)
+         */
+//        Thread {
+//            // Create and open a URL connection and cast it as HttpURLConnection
+//            val imageUrl = URL("https://www.pngkit.com/png/detail/17-176284_eagle-owl-png-transparent-image-owl-png.png")
+//            val connection = imageUrl.openConnection() as HttpURLConnection
+//            connection.doInput = true // This connection is for input only
+//            connection.connect()
+//
+//            // Create an inputStream to read the image from the server to your app
+//            val inputStream = connection.inputStream
+//            // Decode the inputStream into a bitmap using BitmapFactory
+//            val bitmap = BitmapFactory.decodeStream(inputStream)
+//
+//            /**
+//             * Only the MainThread (UI Thread) can update the UI in Android
+//             * so we switch to the UI thread to display our image i.e
+//             * we post the data to the UI thread. There are several methods used
+//             * in doing this.
+//             */
+//
+//            // 1. This method is only available inside an activity
+//        //    runOnUiThread { image.setImageBitmap(bitmap) }
+//
+//            // 2. Using Handler with MainLooper
+//            /**
+//             * Loopers are pieces of the Android ecosystem which loops through all
+//             * messages or signals any thread receives and process them.
+//             * The MainThread Looper or simply MainLooper handles messages for the
+//             * MainThread.
+//             *
+//             * Handlers on the other hand are used to send messages to the loopers.
+//             *
+//             * The combination of handlers and loopers forms a relationship or connection
+//             * between signals and threads that should process them
+//             */
+//            Handler(mainLooper).post { image.setImageBitmap(bitmap) }
+//
+//        }.start()  // Start the thread
 
     }
+
+    /**
+     *                               COROUTINES
+     *  Working threads is a bit tedious and tiring as one can easily forget to goto
+     *  the background thread for some operations and return back to the main thread to use the
+     *  the result obtained from the task from the background processing. This is where Kotlin
+     *  Coroutines come to the rescue.
+     *
+     *  Coroutines allows us to do both background processing and posting to the main thread at the
+     *  same time. Coroutines are functions which can run in parallel and can also be cancelled
+     *  once started unlike regular functions. They are Asynchronous by default and can also be
+     *  paused and resumed at any time i.e They can be suspended.
+     *
+     *  Coroutines required two things to work:
+     *  1. CoroutineScope -> Determines how long the coroutine lives. It is the way of attaching
+     *     the coroutine to a lifecycle. If coroutines are not attached to lifecycle, we would run
+     *     into memory leaks, and have coroutines which never finish.
+     *
+     *  2. Coroutine Builder -> Determines the way the coroutine is launched. It is used to start
+     *     coroutines and wraps code in a coroutine similar to using Threads & Runnables.
+     *
+     *  A Dispatcher is a used to switch threads in coroutines similar to Handler + Looper combination
+     */
 }
