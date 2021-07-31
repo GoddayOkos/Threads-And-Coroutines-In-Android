2993b6db9d9620c3a15ccdeb1df864b961468d6d
Godday Okoduwa <goddayokos@gmail.com>
2021-07-31 10:43:06 +0100

    Completed implementations of JobScheduler for downloading and displaying images

--- app/src/main/java/dev/decagon/godday/threadsandcoroutines/MainActivity.kt
+++ app/src/main/java/dev/decagon/godday/threadsandcoroutines/MainActivity.kt
@@ -1,15 +1,23 @@
 package dev.decagon.godday.threadsandcoroutines
 
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
 import android.content.IntentFilter
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.PersistableBundle
+import androidx.annotation.RequiresApi
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
 import java.net.HttpURLConnection
 import java.net.URL
+
+const val JOB_ID = 10
 
 /**
  * Main Screen
@@ -19,6 +27,7 @@ class MainActivity : AppCompatActivity() {
 
     private val receiver by lazy { ImageDownloadReceiver { displayImage(it) } }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         // Switch to AppTheme for displaying the activity
         setTheme(R.style.AppTheme)
@@ -26,56 +35,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Get a reference to the MainLooper
-        val mainLooper = mainLooper  // or Looper.getMainLooper()
-
         registerReceiver(receiver, IntentFilter().apply {
             addAction(ACTION_IMAGE_DOWNLOAD)
         })
 
-        /**
-         * Android forbids making network calls or connections on MainThread in
-         * other to avoid blocking it. So to download our image, we have to switch the
-         * operation to another thread (Background Thread or Worker Thread)
-         */
-        Thread {
-            // Create and open a URL connection and cast it as HttpURLConnection
-            val imageUrl = URL("https://www.pngkit.com/png/detail/17-176284_eagle-owl-png-transparent-image-owl-png.png")
-            val connection = imageUrl.openConnection() as HttpURLConnection
-            connection.doInput = true // This connection is for input only
-            connection.connect()
+        val jobScheduler = getSystemService(JobScheduler::class.java) ?: return
 
-            // Create an inputStream to read the image from the server to your app
-            val inputStream = connection.inputStream
-            // Decode the inputStream into a bitmap using BitmapFactory
-            val bitmap = BitmapFactory.decodeStream(inputStream)
-
-            /**
-             * Only the MainThread (UI Thread) can update the UI in Android
-             * so we switch to the UI thread to display our image i.e
-             * we post the data to the UI thread. There are several methods used
-             * in doing this.
-             */
-
-            // 1. This method is only available inside an activity
-        //    runOnUiThread { image.setImageBitmap(bitmap) }
-
-            // 2. Using Handler with MainLooper
-            /**
-             * Loopers are pieces of the Android ecosystem which loops through all
-             * messages or signals any thread receives and process them.
-             * The MainThread Looper or simply MainLooper handles messages for the
-             * MainThread.
-             *
-             * Handlers on the other hand are used to send messages to the loopers.
-             *
-             * The combination of handlers and loopers forms a relationship or connection
-             * between signals and threads that should process them
-             */
-            Handler(mainLooper).post { image.setImageBitmap(bitmap) }
-
-        }.start()  // Start the thread
-
+        jobScheduler.schedule(
+            JobInfo.Builder(
+                JOB_ID, ComponentName(this, DownloadImageJobService::class.java)
+            )
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+                .setExtras(PersistableBundle().apply {
+                    putString("image_path", "https://www.wallpaperup.com/uploads/wallpapers/2013/03/21/55924/3b61c716155c6fa88f321da6d4655767.jpg")
+                })
+                .setOverrideDeadline(1500)
+                .build()
+        )
     }
 
     override fun onDestroy() {
